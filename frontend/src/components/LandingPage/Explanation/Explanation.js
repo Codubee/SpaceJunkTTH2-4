078cb58ca5957d06938205a363d6a8203f5b7cb7
Exenny <noviium.exe@gmail.com>
2020-11-09 20:30:15 -0600

    Created Explanation component with Title and description of project

---
+++ frontend/src/components/LandingPage/Explanation/Explanation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Jumbotron} from 'reactstrap';
+import './Explanation.css';
+
+function Explanation() {
+
+    return (<Jumbotron className='Jumbo1'>
+    <h1 className='title'>SPACE JUNK</h1>
+    <p id="desc">The sky's the limit with Space Junk, a web application designed to recommend you songs based on NASA's picture of the day along with some cool space facts. </p>
+    </Jumbotron>);
+
+
+}
+
+export default Explanation;
