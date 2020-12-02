96506e2f5a723c5b4728276c45ff7842fc17115b
Katy <ktho2012@gmail.com>
2020-12-02 15:55:37 -0600

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,20 @@ app.get('/',(req,res)=>{
     res.send('Hello world');
 })
 
+//Get weather on Mars
+app.get('/mars_weather', (req, res) => {
+    
+    axios.get(`https://api.nasa.gov/insight_weather/?api_key=${process.env.MARS_WEATHER_API_KEY}&feedtype=json&ver=1.0`)
+    .then((response) => {
+        console.log(response.data);
+        res.status(200).json(response.data);
+    })
+    .catch((error) => {
+        console.log(error);
+        res.status(400).json({error:"An error occurred"});
+    });
+});
+
 app.listen(port,()=>{
     console.log('API is up and running')
 })
