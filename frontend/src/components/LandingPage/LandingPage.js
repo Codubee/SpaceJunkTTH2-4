a437e93c0baae72acf3b32b623da0a98da729cfb
emart870 <72898341+emart870@users.noreply.github.com>
2020-12-09 00:27:59 -0600

    CB-2 Tools Component (#2)
    * First commit for Tools Commponent
    
    * Second Commit from Tools Component. Added more Tool ojects to make the code more clear
    
    * 3rd commit from Tools Component. Note that the colors used for temportary and can change depending on that we want to go with
    
    * 4th Commit from Tools Componenet. Got rid of unnecessary example code
    
    * 4th commit from Tools Component. Removed Tool Object and instead used a .js file that contains a const
    
    Co-authored-by: Miguel <info@codubee.com>

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import Tools from './Tools/Tools'
+import {ToolsInfo} from './Tools/ToolsInfo'
 import Explanation from './Explanation/Explanation'
 import TeamComponent from './TeamComponent/TeamComponent'
 
-//Import components that you create here
-
 function LandingPage(){
+    
+    //Turns all of those Tool objects into collapse components using the Tools component
     return(
         <div>
             <Explanation/>
+            <Tools toolsList = {ToolsInfo}/>
             <TeamComponent />
         </div>
     )
