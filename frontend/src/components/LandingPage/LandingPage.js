e6946999ec3a3bd0aeaff53a9565ba70051360db
Noviius <62228345+Noviius@users.noreply.github.com>
2020-12-09 00:23:20 -0600

    CB-1 Explanation component (#3)
    * Created Explanation component with Title and description of project
    
    * Updated description and title. Changed color, size, and placing of text to match our theme, still deciding on the background for title
    
    * Changed Jumbotron background to simple color, indented code properly to increase readability
    
    Co-authored-by: Miguel <info@codubee.com>

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Explanation from './Explanation/Explanation'
 import TeamComponent from './TeamComponent/TeamComponent'
+
 //Import components that you create here
 
 function LandingPage(){
     return(
-        //Use components that you import here
-        <TeamComponent />
+        <div>
+            <Explanation/>
+            <TeamComponent />
+        </div>
     )
 }
 
