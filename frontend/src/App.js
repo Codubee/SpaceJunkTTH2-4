620ab47f048dbe3b0d36951a41f7c1fb2e2e3012
Miguel <info@codubee.com>
2020-12-22 14:48:37 -0600

    Added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar'
 import LandingPage from './components/LandingPage/LandingPage'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 function App() {
   return (
     <div>
-      <NavBar/>
-      <LandingPage/>
+      <NavBar />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={LandingPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
