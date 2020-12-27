322a044e6effb20e834dc2676b3f06430ca00ec0
Codubee <70549347+Codubee@users.noreply.github.com>
2020-12-27 14:35:57 -0600

    Cb 12 Call mars photo api (#9)
    * Initial commit
    
    * Changed json response
    
    * Changed API to Mar rover photo
    
    Co-authored-by: Katy <ktho2012@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,27 @@ app.get('/',(req,res)=>{
     res.send('Hello world');
 })
 
+
+//Get Mars photo
+app.get('/mars_photo', (req, res) => {
+    //Get a random Mars day to pull a photo from
+    const sol = Math.floor(Math.random() * Math.floor(2979));
+
+    axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&api_key=${process.env.MARS_WEATHER_API_KEY}`)
+    .then((response) => {
+        //get photo url to send back to front end
+        const photo = response.data.photos[0].img_src;
+        //send photo to front end
+        res.send(photo);
+    })
+    .catch((error) => {
+        console.log(error);
+        res.status(400).json({error:"An error occurred"});
+    });
+    
+});
+
+
 //req == body with contains the imageURL aka the url of the image we want to analyze
 app.post('/imageAnalyzer', (req, res) =>{
 
