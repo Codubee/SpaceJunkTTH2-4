91d4936eed923ed738224d04f7f49a047c6c9e3b
Annika Hendrickson <alh190000@utdallas.edu>
2021-01-04 16:46:24 -0600

    added calls to the NeoWS feed & browse api's

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,8 @@ app.use(express.json())
 
 const port = 8080;
 
-app.get('/asteroid_news', (req,res) =>{
-    const START_DATE = 2020-12-10;
-    const END_DATE = 2020-12-17;
-    axios.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2020-12-10&end_date=2020-12-17&api_key=${process.env.NEWS_API_KEY}')
+app.get('/asteroid_news_feed', (req,res) =>{
+    axios.get('https://api.nasa.gov/neo/rest/v1/feed/today?detailed=true&api_key='+process.env.NEWS_API_KEY)
     .then((response)=>{
         console.log(response.data);
         res.status(200).json(response.data);
@@ -18,7 +16,18 @@ app.get('/asteroid_news', (req,res) =>{
         console.log(error)
         res.status(400).json({error:'An error ocurred.'})
     })
-    //res.send('Hello world');
+})
+
+app.get('/asteroid_news_browse', (req,res) =>{
+    axios.get('https://api.nasa.gov/neo/rest/v1/neo/browse?api_key='+process.env.NEWS_API_KEY)
+    .then((response)=>{
+        console.log(response.data);
+        res.status(200).json(response.data);
+    })
+    .catch((error)=>{
+        console.log(error)
+        res.status(400).json({error:'An error ocurred.'})
+    })
 })
 
 app.listen(port,()=>{
